de852451cef0bed676f3ce57cd1441bf95baf4dd
imaginary <epiccool238@gmail.com>
2025-05-09 16:52:46 +0530

    Update cursed.cpp

--- cursed.cpp
+++ cursed.cpp
@@ -5,6 +5,10 @@
 #include <cstdint>
 #include <vector>
 
+/*
+  I MADE THIS AS A JOKE. DO NOT USE THIS IN ACTUAL CODE
+*/
+
 typedef std::vector<uint8_t> ArrayBuffer;
 
 class DataView {
@@ -49,6 +53,64 @@ public:
 
   void setInt64(int offset, int64_t value) {
     std::memcpy(&buffer[offset], &value, sizeof(int64_t));
+  }
+
+  uint8_t getUint8(int offset) {
+    return buffer[offset];
+  }
+
+  uint16_t getUint16(int offset) {
+    uint16_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(uint16_t));
+    return value;
+  }
+
+  uint32_t getUint16(int offset) {
+    uint32_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(uint32_t));
+    return value;
+  }
+
+  uint64_t getUint16(int offset) {
+    uint64_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(uint64_t));
+    return value;
+  }
+
+  float getFloat32(int offset) {
+    float value;
+    std::memcpy(&value, &buffer[offset], sizeof(float));
+    return value;
+  }
+
+  double getFloat64(int offset) {
+    double value;
+    std::memcpy(&value, &buffer[offset], sizeof(double));
+    return value;
+  }
+
+  int8_t getInt8(int offset) {
+    int8_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(int8_t));
+    return value;
+  }
+
+  int16_t getInt8(int offset) {
+    int16_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(int16_t));
+    return value;
+  }
+
+  int32_t getInt8(int offset) {
+    int32_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(int32_t));
+    return value;
+  }
+
+  int64_t getInt8(int offset) {
+    int64_t value;
+    std::memcpy(&value, &buffer[offset], sizeof(int64_t));
+    return value;
   }
 
 private:
